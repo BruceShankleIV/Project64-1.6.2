978857ee1a976f457a6479c2c953507b2e1fa313
Bruce Shankle IV <bruceiv.shankle@gmail.com>
2026-01-10 18:21:44 -0500

    Need even more translation updates.
    V20 217 is "The game may crash at the upcoming r4300i_SPECIAL_BREAK. Do you want to emulate this potential crash?"
    
    V20 218 is "Potential Upcoming Crash"

--- BruceIV's Project64 1.6.2/Visual Studio Source Code/Main.h
+++ BruceIV's Project64 1.6.2/Visual Studio Source Code/Main.h
@@ -34,7 +34,7 @@ extern "C" {
 #include "Settings API.h"
 #include "ROM Browser.h"
 #include "Language.h"
-/********* Source Version 20 *********/
+/********* Source Version 21 *********/
 /********* General Defaults **********/
 #define AppName						"BruceIV's Project64 1.6.2"
 #define IniName						"Game.ini"
