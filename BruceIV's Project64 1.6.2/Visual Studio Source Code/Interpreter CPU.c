4dc7cf48d0451725f9b1898e0034987634d12aca
Bruce Shankle IV <bruceiv.shankle@gmail.com>
2026-01-13 17:04:16 -0500

    Okay think that's what was missing for interp perf

--- BruceIV's Project64 1.6.2/Visual Studio Source Code/Interpreter CPU.c
+++ BruceIV's Project64 1.6.2/Visual Studio Source Code/Interpreter CPU.c
@@ -417,5 +417,5 @@ void StartInterpreterCPU (void) {
 void TestInterpreterJump (DWORD PC,DWORD TargetPC,int Reg1,int Reg2) {
 	if (PC!=TargetPC) return;
 	if (DelaySlotEffectsCompare(PC,Reg1,Reg2)) return;
-	if (CpuRecompiler) return;
+	if (!CpuRecompiler) InPermLoop();
 }
